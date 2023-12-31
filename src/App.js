0dcf3dc86c888ec2d7dd6e7c1ad4e07dff91d6e8
JulianoAlfredo <juliano.alfredo86@gmail.com>
2023-12-31 15:34:27 -0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import iconGithub from './img/icons8-github.svg'
+import iconLinkedin from './img/icons8-linkedin.svg'
+import iconEmail from './img/icons8-email-48 (1).png'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <div className="header-name">
+          <h1>Juliano Alfredo</h1>
+        </div>
+
+        <div className="header-about">
+          <h5>Sobre</h5>
+          <h5>Resumo</h5>
+        </div>
       </header>
+
+      <div className="content">
+        <div className="section">
+          <h3>Formação</h3>
+          <p>Engenharia da Computação</p>
+        </div>
+
+        <div className="section">
+          <h3>Tecnologias</h3>
+          <p>Java && NodeJS</p>
+        </div>
+
+        <div className="section">
+          <div>
+            <h3>Contatos</h3>
+          </div>
+          <div>
+            <div className="specify-contact">
+             
+             
+                <a href="https://github.com/JulianoAlfredo"> <img src={iconGithub} /></a>
+             
+             
+                <a href="https://www.linkedin.com/in/juliano-figueiredo-3129b71ab/"><img src={iconLinkedin} /></a>
+             
+            
+                <a href="mailto:juliano.alfredo86@gmail.com"><img src={iconEmail} /></a>
+
+            </div>
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
